7db730a49ee9bcf43b4d1640cb514d57d0ff4ae3
Tomas Jedlicka <jedlickat@gmail.com>
2017-12-14 09:28:39 -0500

    Iterate over all available probes

--- dtrace.go
+++ dtrace.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jedlickat/libdtrace-go"
 )
 
 func main() {
 	handle := dtrace.NewDTraceHandle()
-	dtrace.GetProbes(handle)
+	fmt.Println("DTrace-go")
+	fmt.Println("available probes:")
+	for _, probe := range dtrace.GetProbes(handle) {
+		fmt.Printf("%d: %s: %s: %s: %s\n", probe.ProbeID,
+			probe.ProviderName, probe.ModuleName,
+			probe.FunctionName, probe.ProbeName);
+	}
 	dtrace.FreeDTraceHandle(handle)
 }
